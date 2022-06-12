0197ae7ff969b78423bc516aef44d54d23c968ef
Anastasia Sergeeva <lirebook@yandex.ru>
2022-06-12 18:36:02 +0300

    bugfix: исправить ошибки в index.js

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
+// объявляем переменные для открытия и закрытия попапа
 const popup = document.querySelector('.popup');
 const editBtn = document.querySelector('.button_type_edit');
 const closeBtn = document.querySelector('.button_type_close');
-let formElement = document.querySelector('.form');
-let nameInput = document.querySelector('.form__input_type_name');
-let jobInput = document.querySelector('.form__input_type_job');
 
-editBtn.addEventListener('click', () => {
+// объявляем переменные для элементов формы
+const formElement = document.querySelector('.form');
+let nameInput = formElement.querySelector('.form__input_type_name');
+let jobInput = formElement.querySelector('.form__input_type_job');
+
+// объявляем переменные для элементов данных пользователя
+const profile = document.querySelector('.profile');
+let profileJob = profile.querySelector('.profile__description');
+let profileName = profile.querySelector('.profile__name');
+
+// функция открытия попапа при нажатии на кнопку редактирования
+function openPopup() {
     popup.classList.add('popup_opened');
-});
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+}
 
-closeBtn.addEventListener('click', () => {
-    popup.classList.remove('popup_opened');
-});
+editBtn.addEventListener('click', openPopup);
 
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    let profileJob = document.querySelector('.profile__description');
-    let profileName = document.querySelector('.profile__name');
-    profileJob.textContent = jobInput.value;
-    profileName.textContent = nameInput.value;
-
+// функция закрытия попапа при нажатии на крестик
+function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
-formElement.addEventListener('submit', formSubmitHandler); 
+closeBtn.addEventListener('click', closePopup);
+
+// функция сохранения введенных данных в попап
+function submitForm (evt) {
+    evt.preventDefault(); 
+    profileJob.textContent = jobInput.value;
+    profileName.textContent = nameInput.value;
+
+    closePopup();
+}
+
+formElement.addEventListener('submit', submitForm); 
