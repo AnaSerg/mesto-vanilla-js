16f4a3b23fb685492f5181d61ba10859b203f873
Anastasia Sergeeva <lirebook@yandex.ru>
2022-06-25 02:19:16 +0300

    feat: сделать попап добавления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,11 @@
-// объявляем переменные для открытия и закрытия попапа
-const popup = document.querySelector('.popup');
+// попапы
+const popupEdit = document.querySelector('.popup_type_edit');
+const popupAdd = document.querySelector('.popup_type_add');
+
+// кнопки
 const editBtn = document.querySelector('.button_type_edit');
-const closeBtn = document.querySelector('.button_type_close');
+const addBtn = document.querySelector('.button_type_add');
+const closeBtn = document.querySelectorAll('.button_type_close');
 
 // объявляем переменные для элементов формы
 const formElement = document.querySelector('.form');
@@ -13,29 +17,67 @@ const profile = document.querySelector('.profile');
 let profileJob = profile.querySelector('.profile__description');
 let profileName = profile.querySelector('.profile__name');
 
-// функция открытия попапа при нажатии на кнопку редактирования
-function openPopup() {
+
+const elementsList = document.querySelector('.elements-list');
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+
+// открытие попапов
+const openPopup = (popup) => {
     popup.classList.add('popup_opened');
+};
+
+const handleEditProfile = () => {
+    openPopup(popupEdit);
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
-}
+};
 
-editBtn.addEventListener('click', openPopup);
+const addCard = () => {
+    openPopup(popupAdd);
+};
 
-// функция закрытия попапа при нажатии на крестик
-function closePopup() {
-    popup.classList.remove('popup_opened');
-}
+editBtn.addEventListener('click', handleEditProfile);
+addBtn.addEventListener('click', addCard);
 
-closeBtn.addEventListener('click', closePopup);
+// закрытие попапов
+closeBtn.forEach(item => {
+    item.addEventListener('click', function (event) {
+        const popup = event.target.closest('.popup_opened');
+        popup.classList.remove('popup_opened');
+    });
+});
 
 // функция сохранения введенных данных в попап
 function submitForm (evt) {
     evt.preventDefault(); 
     profileJob.textContent = jobInput.value;
     profileName.textContent = nameInput.value;
-
-    closePopup();
+    popupEdit.classList.remove('popup_opened');
 }
 
 formElement.addEventListener('submit', submitForm); 
