fd38a6fb6111bd9d760505ac4544dae39d1ced72
Anastasia Sergeeva <lirebook@yandex.ru>
2022-06-25 09:40:40 +0300

    feat: сделать отображение карточек из коробки

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ const addBtn = document.querySelector('.button_type_add');
 const closeBtn = document.querySelectorAll('.button_type_close');
 
 // объявляем переменные для элементов формы
-const formElement = document.querySelector('.form');
+const formElement = document.querySelector('.form_type_edit');
 let nameInput = formElement.querySelector('.form__input_type_name');
 let jobInput = formElement.querySelector('.form__input_type_job');
 
@@ -17,34 +17,44 @@ const profile = document.querySelector('.profile');
 let profileJob = profile.querySelector('.profile__description');
 let profileName = profile.querySelector('.profile__name');
 
-
+// массив с карточками и добавление карточек при загрузке страницы
 const elementsList = document.querySelector('.elements-list');
+const elementTemplate = document.querySelector('.elements-template').content;
+
 const initialCards = [
     {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+      name: 'Нью-Йорк',
+      link: 'https://images.unsplash.com/photo-1562649375-10a87a607863?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGNpdGllcyUyMG5ldyUyMHlvcmt8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
     },
     {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+      name: 'Барселона',
+      link: 'https://images.unsplash.com/photo-1622649440704-7f65fef23d25?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNpdGllcyUyMHN0cmVldHN8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
     },
     {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+      name: 'Сан-Франциско',
+      link: 'https://images.unsplash.com/photo-1564508940884-2069e7943cc3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bW9zY293JTIwc3RyZWV0fGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60'
     },
     {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+      name: 'Берлин',
+      link: 'https://images.unsplash.com/photo-1588344948644-097b9a9bc3c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGJ1aWxkaW5ncyUyMGJlcmxpbnxlbnwwfDJ8MHx8&auto=format&fit=crop&w=800&q=60'
     },
     {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+      name: 'Париж',
+      link: 'https://images.unsplash.com/photo-1530029619632-57d6123f3537?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y2l0aWVzJTIwcGFyaXN8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
     },
     {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+      name: 'Венеция',
+      link: 'https://images.unsplash.com/photo-1575971252847-77ade1fa4997?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YnVpbGRpbmdzJTIwdmVuaWNlfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60'
     }
   ]; 
+
+initialCards.forEach(function (el) {
+    const element = elementTemplate.cloneNode(true);
+    element.querySelector('.element__text').textContent = el.name;
+    element.querySelector('.element__image').src = el.link;
+
+    elementsList.append(element);
+});
 
 // открытие попапов
 const openPopup = (popup) => {
@@ -72,7 +82,7 @@ closeBtn.forEach(item => {
     });
 });
 
-// функция сохранения введенных данных в попап
+// функция сохранения введенных данных профиля в попап
 function submitForm (evt) {
     evt.preventDefault(); 
     profileJob.textContent = jobInput.value;
@@ -81,3 +91,4 @@ function submitForm (evt) {
 }
 
 formElement.addEventListener('submit', submitForm); 
+
