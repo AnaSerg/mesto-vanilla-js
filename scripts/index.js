bbddd1d2a1cbec847e3527c7962b41e0b7c87f9d
Anastasia Sergeeva <lirebook@yandex.ru>
2022-06-27 14:57:50 +0300

    bugfix: исправить ошибки 1-ой итерации

--- scripts/index.js
+++ scripts/index.js
@@ -26,59 +26,16 @@ const profileName = profile.querySelector('.profile__name');
 const image = document.querySelector('.popup__image');
 const imageDescription = document.querySelector('.popup__image-description');
 
-const initialCards = [
-    {
-      name: 'Нью-Йорк',
-      link: 'https://images.unsplash.com/photo-1634471696242-d447b79248a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fCVEMCVCRCVEMSU4QyVEMSU4RSUyMCVEMCVCOSVEMCVCRSVEMSU4MCVEMCVCQXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
-    },
-    {
-      name: 'Барселона',
-      link: 'https://images.unsplash.com/photo-1511527661048-7fe73d85e9a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YmFyY2Vsb25hfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
-    },
-    {
-      name: 'Сан-Франциско',
-      link: 'https://images.unsplash.com/photo-1521464302861-ce943915d1c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2FuJTIwZnJhbmNpc2NvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
-    },
-    {
-      name: 'Берлин',
-      link: 'https://images.unsplash.com/photo-1560969184-10fe8719e047?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmVybGlufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
-    },
-    {
-      name: 'Париж',
-      link: 'https://images.unsplash.com/photo-1530029619632-57d6123f3537?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y2l0aWVzJTIwcGFyaXN8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
-    },
-    {
-      name: 'Венеция',
-      link: 'https://images.unsplash.com/photo-1601221018091-03efdfe0ba53?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fCVEMCVCMiVEMCVCNSVEMCVCRCVEMCVCNSVEMSU4NiVEMCVCOCVEMSU4RnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
-    }
-  ]; 
-
-// добавление карточек на страницу
-
 const elementsList = document.querySelector('.elements-list');
 const elementTemplate = document.querySelector('.elements-template').content;
 
-const renderCards = () => {
-    initialCards.forEach(renderCard);
-};
-
-// удаление карточки
-
-const deleteCard = () => {
-    const deleteBtns = document.querySelectorAll('.element__delete-button');
-    deleteBtns.forEach(item => {
-        item.addEventListener('click', function (event) {
-            const card = event.target.closest('.element');
-            card.remove();
-        });
-    });
-};
 
 // увеличение изображений
 
 const zoomImage = (imageLink, imageText, zoomBtn) => {
     zoomBtn.addEventListener('click', function () {
         image.src = imageLink;
+        image.alt = imageText;
         imageDescription.textContent = imageText;
         openPopup(popupImage);
     });
@@ -92,19 +49,36 @@ const likeImage = (likeBtn) => {
     });
 };
 
-// создание карточки
+// удаление карточки
 
-const renderCard = ({name, link}) => {
+const deleteCard = (deleteBtn) => { 
+    deleteBtn.addEventListener('click', function (event) { 
+        const card = event.target.closest('.element'); 
+        card.remove(); 
+    }); 
+}; 
+
+// создание карточек и добавление на страницу
+
+const renderCards = () => {
+    initialCards.forEach(renderCard);
+};
+
+const renderCard = (data) => {
+    const card = createCard(data);
+    elementsList.prepend(card);
+};
+
+const createCard = ({name, link}) => {
     const element = elementTemplate.cloneNode(true);
     element.querySelector('.element__text').textContent = name;
     element.querySelector('.element__image').src = link;
+    element.querySelector('.element__image').alt = name;
+    const deleteBtn = element.querySelector('.element__delete-button'); // обращение к кнопке удаления
     const imageBtn = element.querySelector('.element__image'); // обращение к картинке для увеличения
+    
     likeImage(element.querySelector('.element__like')); // лайк
-
-    elementsList.prepend(element);
-
-    deleteCard();
-
+    deleteCard(deleteBtn);
     zoomImage(link, name, imageBtn);
 
     return element;
@@ -124,12 +98,12 @@ const handleEditProfile = () => {
     jobInput.value = profileJob.textContent;
 };
 
-const addCard = () => {
+const openAddCardPopup = () => {
     openPopup(popupAdd);
 };
 
 editBtn.addEventListener('click', handleEditProfile);
-addBtn.addEventListener('click', addCard);
+addBtn.addEventListener('click', openAddCardPopup);
 
 // закрытие попапов
 
