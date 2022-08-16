c67964bc68be67762b81b052531eb809dc77ad39
Anastasia Sergeeva <lirebook@yandex.ru>
2022-08-16 23:49:32 +0300

    refactor: добавить класс PopupWithImage

---
+++ scripts/components/PopupWithImage.js
@@ -0,0 +1,16 @@
+import { Popup } from './Popup.js';
+
+export class PopupWithImage extends Popup {
+    constructor(popupSelector) {
+        super(popupSelector);
+        this._popupImage = document.querySelector('.popup__image');
+        this._imageDescription = document.querySelector('.popup__image-description');
+    }
+
+    open (name, link) {
+        this._popupImage.src = link;
+        this._popupImage.alt = name;
+        this._imageDescription.textContent = name;
+        super.open();
+    }
+}
