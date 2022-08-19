76672049bb4aad01d109f61e06ba0eab58fca52d
Anastasia Sergeeva <lirebook@yandex.ru>
2022-08-19 07:19:00 +0300

    refactor: сборка Webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import '../pages/index.css';
 import { initialCards,
         elementList,
         buttonEdit,
@@ -7,13 +8,13 @@ import { initialCards,
         nameInput,
         jobInput,
         config
-} from './utils/constants.js'
-import { Card } from './components/Сard.js';
-import { FormValidator } from './components/FormValidator.js';
-import { Section } from './components/Section.js';
-import { PopupWithImage } from './components/PopupWithImage.js';
-import { PopupWithForm } from './components/PopupWithForm.js';
-import { UserInfo } from './components/UserInfo.js';
+} from '../scripts/utils/constants.js'
+import { Card } from '../scripts/components/Сard.js';
+import { FormValidator } from '../scripts/components/FormValidator.js';
+import { Section } from '../scripts/components/Section.js';
+import { PopupWithImage } from '../scripts/components/PopupWithImage.js';
+import { PopupWithForm } from '../scripts/components/PopupWithForm.js';
+import { UserInfo } from '../scripts/components/UserInfo.js';
 
 const formEditValidation = new FormValidator(config, formElementEdit);
 const formAddValidation = new FormValidator(config, formElementAdd);
